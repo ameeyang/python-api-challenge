620c0052e43036775158c00d6000dd277955e45d
Amee Yang <amee979@gmail.com>
2021-01-09 00:52:44 +0000

    Upload New File

---
+++ WeatherPy.ipynb/api_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 22:51:57 2021
+
+@author: ameeyang
+"""
+#API KEY
+weather = 'c5df9e845df0bd6d2e129f3d15cc589f'
+g_key = 'AIzaSyB81_1guycwxITyNlmh-i5XrRluww6k0qw'
